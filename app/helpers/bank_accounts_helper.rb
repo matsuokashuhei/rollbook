5d56ed9f8512657766b241208a9f057b2d34a568
Shuhei Matsuoka <shooohei.m@gmail.com>
2013-09-20 08:46:09 +0900

    会員と口座を連携する。

--- app/helpers/bank_accounts_helper.rb
+++ app/helpers/bank_accounts_helper.rb
@@ -1,11 +1,28 @@
 module BankAccountsHelper
 
+  STATUS = {
+    "0" => "未登録",
+    "1" => "手続き中",
+    "2" => "登録済み",
+    "3" => "手続き失敗",
+    "4" => "マスター削除中",
+    "5" => "マスター削除済"
+  }
+
   def display_name(bank_account)
     <<-"EOS".strip_heredoc.html_safe
     <small>#{bank_account.holder_name_kana}</small><br />
     #{bank_account.holder_name}
     EOS
     #"<small>#{bank_account.holder_name_kana}</small><br />#{bank_account.holder_name}".html_safe
+  end
+
+  def status_name(status)
+    STATUS[status]
+  end
+
+  def statuses
+    STATUS
   end
 
 end
