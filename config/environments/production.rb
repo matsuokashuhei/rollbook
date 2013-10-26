fc850dad40d3b29a71734649181354ece4a02886
Shuhei Matsuoka <shooohei.m@gmail.com>
2013-10-26 20:29:45 +0900

    アイコンを変更する。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,7 @@ Rollbook::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  #config.serve_static_assets = false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -61,7 +60,6 @@ Rollbook::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( .svg, .eot, .woff, .ttf )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
