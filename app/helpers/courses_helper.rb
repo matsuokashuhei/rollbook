ec1e2d4c4d291b4e189ae732b1b2ddcf190eb773
Shuhei Matsuoka <shooohei.m@gmail.com>
2013-11-04 15:07:01 +0900

    クラスを仕上げる。

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,11 +1,12 @@
 module CoursesHelper
 
+  # パン屑用
   def list_item_to_schools(active: false)
-    text = t("activerecord.models.school")
+    text = t("activerecord.models.course")
     if active
       content_tag :li, text, class: "active"
     else
-      content_tag :li, schools_link
+      content_tag :li, link_to_schools
     end
 
   end
@@ -26,21 +27,53 @@ module CoursesHelper
     if active
       content_tag :li, text, class: "active"
     else
-      content_tag :li, course_link(course)
+      content_tag :li, link_to_course(course)
     end
 
   end
 
-  def schools_link
-    link_to t("activerecord.models.school"), schools_path
-  end
-
-  def courses_link(studio, date: Date.today)
-    if date.present?
-      link_to "#{studio.school.name}#{studio.name}", courses_path(studio_id: studio.id, date: date.strftime("%Y%m%d"))
+  # タブ用
+  def tab_to_course course, active: false
+    text = t("activerecord.models.course")
+    if active
+      content_tag :li, class: "active" do
+        link_to text, '#course', data: { toggle: "tab" }
+      end
     else
-      link_to "#{studio.school.name}#{studio.name}", courses_path(studio_id: studio.id, date: Date.today.strftime("%Y%m%d"))
+      content_tag :li do
+        link_to text, course_path(course)
+      end
     end
+  end
+
+  def tab_to_courses_members course, active: false
+    text = t("activerecord.models.member")
+    if active
+      content_tag :li, class: "active" do
+        link_to text, '#member', data: { toggle: "tab" }
+      end
+    else
+      content_tag :li do
+        link_to text, course_members_path(course)
+      end
+    end
+  end
+
+  def tab_to_courses_lessons course, active: false
+    text = t "activerecord.models.lesson"
+    if active
+      content_tag :li, class: "active" do
+        link_to text, '#lesson', data: { toggle: "tab" }
+      end
+    else
+      content_tag :li do
+        link_to text, course_lessons_path(course)
+      end
+    end
+  end
+
+  def link_to_schools
+    link_to t("activerecord.models.school"), schools_path
   end
 
   def instructor_courses_link(instructor, disable: false)
@@ -51,7 +84,18 @@ module CoursesHelper
     end
   end
 
-  def course_link(course)
+  private
+
+  def link_to_schools
+    link_to t("activerecord.models.course"), schools_path
+  end
+
+  def link_to_courses studio, data: Date.today
+    link_to t("activerecord.models.course"), courses_path(studio_id: studio.id, date: date.strftime("%Y%m%d"))
+  end
+
+  def link_to_course course
     link_to course.decorate.name, course_path(course)
   end
+
 end
