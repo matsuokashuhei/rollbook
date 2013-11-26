3023982d5a9ad017e13a31857414eb34be08e3ea
Shuhei Matsuoka <shooohei.m@gmail.com>
2013-11-27 05:29:30 +0900

    クラスの受講会員一覧とレッスンの会員一覧に書類不備の通知を追加する。

---
+++ app/decorators/members_course_decorator.rb
@@ -0,0 +1,21 @@
+class MembersCourseDecorator < ApplicationDecorator
+  delegate_all
+
+  def introduction
+    if model.introduction
+      tooltip_options = { toggle: "tooltip", "original-title" => h.t("activerecord.attributes.members_course.introduction") }
+      h.content_tag(:span, class: "badge", style: "background-color: greenyellow; font-size: 18px; font-weight: normal;", data: tooltip_options) do
+        h.fa_icon "meh-o"
+      end
+    end
+  end
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
---
+++ spec/decorators/members_course_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe MembersCourseDecorator do
+end
