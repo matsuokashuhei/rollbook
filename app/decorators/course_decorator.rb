f7b965cba0b44e5ff8667c95301c0b3870a2b126
Shuhei Matsuoka <shooohei.m@gmail.com>
2014-01-05 19:03:01 +0900

    クラスの表示をかえるが、それをコメントアウトにする。

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -11,6 +11,26 @@ class CourseDecorator < ApplicationDecorator
     { 1 => "月", 2 => "火", 3 => "水", 4 => "木", 5 => "金", 6 => "土", 7 => "日" }[cwday]
   end
 
+  def name_new
+    timetable = model.timetable
+    time_slot = timetable.time_slot
+    studio = timetable.studio
+    school = studio.school
+    text =  "<ul class='list-unstyled' style='margin-top: 1px; margin-bottom: 1px;'>"
+    text += "<li>"
+    text += h.content_tag :h6, style: "margin-top: 0px; margin-bottom: 0px;" do
+      "#{school.name}#{studio.name}　#{weekday_ja(timetable.weekday)}曜#{time_slot.start_time}〜"
+    end
+    text += "</li>"
+    text += "<li>"
+    text += h.content_tag :h3, style: "margin-top: 0px; margin-bottom: 0px;" do
+      "#{model.dance_style.name}#{model.level.name}"
+    end
+    text += "</li>"
+    text += "</ul>"
+    text.html_safe
+  end
+
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
--- app/decorators/instructor_decorator.rb
+++ app/decorators/instructor_decorator.rb
@@ -2,6 +2,23 @@ class InstructorDecorator < ApplicationDecorator
   delegate_all
 
   def name
+=begin
+    text =  "<ul class='list-unstyled' style='margin-top: 1px; margin-bottom: 1px;'>"
+    text += "<li>"
+    if model.team.present?
+      text += h.content_tag :h6, style: "margin-top: 0px; margin-bottom: 0px;" do
+        "#{model.team}"
+      end
+    text += "</li>"
+    end
+    text += "<li>"
+    text += h.content_tag :h3, style: "margin-top: 0px; margin-bottom: 0px;" do
+      "#{model.name}"
+    end
+    text += "</li>"
+    text += "</ul>"
+    text.html_safe
+=end
     if model.kana.present?
       h.content_tag(:small, model.kana) + h.tag(:br) + model.name
     else
