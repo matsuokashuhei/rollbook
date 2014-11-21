ae58a5c28cd738e2898ea16be2646ad58d8a60c1
matsuosh <shooohei.m@gmail.com>
2014-11-22 01:52:52 +0900

    レッスンの一覧の前月・翌月、前日・翌日のリンクの作るコードを改善する。

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,35 +1,31 @@
 module LessonsHelper
 
   def link_to_prev_months_lessons month
-    prev_month = (month.concat("01").to_date - 1.day).strftime("%Y%m")
-    text = prev_month[0..3].concat("年").concat(prev_month[4..5]).concat("月")
-    link_to lessons_path(month: prev_month), class: "btn btn-link pull-left" do
-      fa_icon "caret-left", text: text
+    end_of_prev_month = Rollbook::Util::Month.beginning_of_month(month) - 1.day
+    link_to lessons_path(month: end_of_prev_month.strftime('%Y%m')), class: "btn btn-link pull-left" do
+      fa_icon("caret-left", text: end_of_prev_month.strftime('%Y年%m月'))
     end
   end
 
   def link_to_next_months_lessons month
-    next_month = (month.concat("01").to_date + 1.month).strftime("%Y%m")
-    text = next_month[0..3].concat("年").concat(next_month[4..5]).concat("月").concat(" ")
-    link_to lessons_path(month: next_month), class: "btn btn-link pull-right" do
-      (text.concat(fa_icon("caret-right"))).html_safe
+    beginning_of_next_month = Rollbook::Util::Month.end_of_month(month) + 1.day
+    link_to lessons_path(month: beginning_of_next_month.strftime('%Y%m')), class: "btn btn-link pull-right" do
+      fa_icon('caret-right', text: beginning_of_next_month.strftime('%Y年%m月'), right: true)
     end
   end
 
-  def link_to_yesterdays_lessons date, school_id
-    yesterday = date - 1.day
-    unless Holiday.exists? date: yesterday
-      link_to lessons_path(date: yesterday.to_s(:number), school_id: school_id), class: "btn btn-link pull-left" do
-        fa_icon "caret-left", text: yesterday.strftime("%m月%d日")
+  def link_to_day_before_lessons date, school_id
+    if Holiday.workday?(date.yesterday)
+      link_to lessons_path(date: date.yesterday.to_s(:number), school_id: school_id), class: "btn btn-link pull-left" do
+        fa_icon("caret-left", text: date.yesterday.strftime("%m月%d日"))
       end
     end
   end
 
-  def link_to_tomorrows_lessons date, school_id
-    tomorrow = date + 1.day
-    unless Holiday.exists? date: tomorrow
-      link_to lessons_path(date: tomorrow.to_s(:number), school_id: school_id), class: "btn btn-link pull-right" do
-        tomorrow.strftime("%m月%d日 ").concat(fa_icon("caret-right")).html_safe
+  def link_to_day_after_lessons date, school_id
+    if Holiday.workday?(date.tomorrow)
+      link_to lessons_path(date: date.tomorrow.to_s(:number), school_id: school_id), class: "btn btn-link pull-right" do
+        fa_icon('caret-right', text: date.tomorrow.strftime('%m月%d日'), right: true)
       end
     end
   end
