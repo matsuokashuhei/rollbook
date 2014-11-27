2806981b214950a3371e0e3ec2f60c5b3223ef69
matsuosh <shooohei.m@gmail.com>
2014-11-27 23:40:55 +0900

    [clean] 出席簿システムが稼働しはじめた年月（２０１３年６月１日）を定数にして、このモジュールで管理することにする。

--- lib/rollbook/util.rb
+++ lib/rollbook/util.rb
@@ -1,6 +1,7 @@
 module Rollbook::Util
     
   module Month
+    FIRST_WORKING_DAY = Date.new(2013, 6, 1)
     # 月の初めの日を返す
     def self.beginning_of_month(month = Date.today.strftime('%Y%m'))
       Date.new(month[0, 4].to_i, month[4, 2].to_i, 1)
@@ -15,13 +16,13 @@ module Rollbook::Util
       (beginning_of_month..beginning_of_month.end_of_month).map {|date| date }
     end
     # これまでの年月を返す。
-    def self.total_worked_months(last_day = Date.today)
-      current_day = Date.new(2013, 6, 1).end_of_month
+    def self.total_worked_months(first_day: FIRST_WORKING_DAY.end_of_month, last_day: Date.today, format: '%Y%m')
+      current_day = first_day
       months = [current_day,]
       while current_day < last_day do
         months << current_day += 1.month
       end
-      months.map {|month| month.strftime('%Y%m') }
+      months.map {|month| month.strftime(format) }
     end
 
   end
