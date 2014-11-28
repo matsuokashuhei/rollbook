a7188afc6e6fcca7da136f7fffc86ee198363fe1
matsuosh <shooohei.m@gmail.com>
2014-11-29 02:28:35 +0900

    [modify] 統計に売り上げを加える。

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,101 +1,117 @@
 class DashboardsController < ApplicationController
+  before_action :set_months, only: [:index]
 
   def index
     @members_chart = members_chart
     @members_courses_chart = members_courses_chart
     @recesses_chart = recesses_chart
+    @sales_chart = sales_chart
   end
   
   private
   
-  def members_chart
-    result = StatisticsQuery.monthly_active_members
-    source = to_chart_source(result)
-    members_chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.xAxis(categories: source[:months])
-      f.yAxis([{ title: { text: "" }, min: 0, }, { title: { text: "", }, min: 0, opposite: true }, ])
-      f.tooltip(value_suffix: '人')
-      f.series(name: "入会", yAxis: 0, type: "column", data: source[:monthly_increase])
-      f.series(name: "退会", yAxis: 0, type: "column", data: source[:monthly_decrease])
-      f.series(name: "在籍", yAxis: 1, type: "line", data: source[:total_changes])
+    def set_months
+      @months = Rollbook::Util::Month.total_worked_months(format: '%Y/%m')
     end
-  end
-
-  def members_courses_chart
-    sum = nil
-    members_courses_chart = LazyHighCharts::HighChart.new('graph') do |f|
-      School.order(:open_date).each do |school|
-        # 検索する。
-        result = StatisticsQuery.monthly_active_members_courses(school_id: school.id)
-        # 検索結果をチャート用に加工する。  
-        source = to_chart_source(result)
-        # チャートに渡す。  
-        f.xAxis(categories: source[:months])
+  
+    def members_chart
+      result = StatisticsQuery.monthly_active_members
+      source = to_chart_source(result)
+      members_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.xAxis(categories: @months)
+        f.yAxis([{ title: { text: "" }, min: 0, }, { title: { text: "", }, min: 0, opposite: true }, ])
+        f.tooltip(value_suffix: '人')
+        f.series(name: "入会", yAxis: 0, type: "column", data: source[:monthly_increase])
+        f.series(name: "退会", yAxis: 0, type: "column", data: source[:monthly_decrease])
+        f.series(name: "在籍", yAxis: 1, type: "line", data: source[:total_changes])
+      end
+    end
+  
+    def members_courses_chart
+      sum_members_courses = @months.map {|month| 0 }
+      members_courses_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.xAxis(categories: @months)
         f.yAxis([{ title: { text: "" }, min: 0, }, { title: { text: "", }, min: 0, opposite: true }, ])
         f.tooltip(value_suffix: 'クラス')
-        f.series(name: school.name, yAxis: 0, type: "column", data: source[:total_changes])
-        if sum.blank?
-          sum = source[:total_changes]
-        else
-          sum = [sum, source[:total_changes]].transpose.map {|num| num.inject(&:+) }
+        School.order(:open_date).each do |school|
+          # 検索する。
+          result = StatisticsQuery.monthly_active_members_courses(school_id: school.id)
+          # 検索結果をチャート用に加工する。  
+          source = to_chart_source(result)
+          # チャートに渡す。  
+          f.series(name: school.name, yAxis: 0, type: "column", data: source[:total_changes])
+          sum_members_courses = [sum_members_courses, source[:total_changes]].transpose.map {|num| num.inject(&:+) }
         end
+        f.series(name: '合計', yAxis: 1, type: "line", data: sum_members_courses)
       end
-      f.series(name: '合計', yAxis: 1, type: "line", data: sum)
     end
-  end
-  
-  
-  def recesses_chart
-    sum_members_courses = nil
-    sum_recesses = nil
-    members_courses_chart = LazyHighCharts::HighChart.new('graph') do |f|
-      School.order(:open_date).each_with_index do |school, i|
-        # 検索する。
-        result = StatisticsQuery.monthly_active_members_courses(school_id: school.id)
-        # 検索結果をチャート用に加工する。  
-        source = to_chart_source(result)
-        active_members_courses = [source[:total_changes], source[:monthly_recesses]].transpose.map {|num| num.inject(&:+) }
-        recess_rate = [source[:monthly_recesses], active_members_courses].transpose.map {|num| ((num[0].to_f / num[1].to_f) * 100).round(1) }
-        # チャートに渡す。  
-        f.xAxis(categories: source[:months])
+    
+    
+    def recesses_chart
+      sum_members_courses = @months.map {|month| 0 }
+      sum_recesses = @months.map {|month| 0 }
+      members_courses_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.xAxis(categories: @months)
         f.yAxis({ title: { text: "" }, min: 0, max: 50 })
         f.tooltip(value_suffix: '%')
-        f.series(name: school.name, yAxis: 0, type: "column", data: recess_rate)
-        
-        if i == 0
-          sum_members_courses = active_members_courses
-          sum_recesses = source[:monthly_recesses]
-        else
+        School.order(:open_date).each_with_index do |school, i|
+          # 検索する。
+          result = StatisticsQuery.monthly_active_members_courses(school_id: school.id)
+          # 検索結果をチャート用に加工する。  
+          source = to_chart_source(result)
+          active_members_courses = [source[:total_changes], source[:monthly_recesses]].transpose.map {|num| num.inject(&:+) }
+          recess_rate = [source[:monthly_recesses], active_members_courses].transpose.map {|num| ((num[0].to_f / num[1].to_f) * 100).round(1) }
+          # チャートに渡す。  
+          f.series(name: school.name, yAxis: 0, type: "column", data: recess_rate)
           sum_members_courses = [sum_members_courses, active_members_courses].transpose.map {|num| num.inject(&:+) }
           sum_recesses = [sum_recesses, source[:monthly_recesses]].transpose.map {|num| num.inject(&:+) }
         end
+        sum_recess_rate = [sum_recesses, sum_members_courses].transpose.map {|num| ((num[0].to_f / num[1].to_f) * 100).round(1) }
+        f.series(name: '合計', yAxis: 0, type: "line", data: sum_recess_rate)
       end
-      sum_recess_rate = [sum_recesses, sum_members_courses].transpose.map {|num| ((num[0].to_f / num[1].to_f) * 100).round(1) }
-      f.series(name: '合計', yAxis: 0, type: "line", data: sum_recess_rate)
     end
-  end
-
-  def to_chart_source(result)
-    # 月
-    months = result.map {|row| "#{row['month'][0, 4]}/#{row['month'][4, 2]}" }
-    # 月別数
-    monthly_increase = result.map {|row| row["monthly_increase"].to_i }
-    monthly_decrease = result.map {|row| row["monthly_decrease"].to_i }
-    monthly_recesses = result.map {|row| row["monthly_recesses"].to_i }
-    # 累積数
-    total_increase = result.map {|row| row["total_increase"].to_i }
-    total_decrease = result.map {|row| row["total_decrease"].to_i }
-    total_changes = [total_increase, total_decrease].transpose.map {|num| num.inject(&:-) }
-    if monthly_recesses.present?
-      total_changes = [total_changes, monthly_recesses].transpose.map {|num| num.inject(&:-) }
+  
+    def sales_chart
+      sales_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.xAxis(categories: @months)
+        sum_sales = @months.map {|month| 0 }
+        f.xAxis(categories: @months)
+        f.yAxis([{ title: { text: "" }, min: 0, }, { title: { text: "", }, min: 0, opposite: true }, ])
+        f.tooltip(value_suffix: '千円')
+        # 店舗の売り上げ
+        School.all.each_with_index do |school, i|
+          # result = StatisticsQuery.sales_report(school_id: school.id)
+          # f.series(name: school.name, yAxis: 0, type: 'column', data: result.map {|row| row["sales"].to_i / 1000 })
+          # sum_sales = [sum_sales, result.map {|row| row["sales"].to_i / 1000 }].transpose.map {|num| num.inject(&:+) }
+          tuition_fees = @months.map {|month| school.tuition_fee(month: month.delete('/')) }
+          f.series(name: school.name, yAxis: 0, type: 'column', data: tuition_fees)
+          sum_sales = [sum_sales, tuition_fees].transpose.map {|num| num.inject(&:+) }
+        end
+        f.series(name: '合計', yAxis: 1, type: "line", data: sum_sales)
+      end
     end
-    {
-      months: months,
-      monthly_increase: monthly_increase,
-      monthly_decrease: monthly_decrease,
-      monthly_recesses: monthly_recesses,
-      total_changes: total_changes
-    }
-  end
-
+  
+    def to_chart_source(result)
+      # 月
+      months = result.map {|row| "#{row['month'][0, 4]}/#{row['month'][4, 2]}" }
+      # 月別数
+      monthly_increase = result.map {|row| row["monthly_increase"].to_i }
+      monthly_decrease = result.map {|row| row["monthly_decrease"].to_i }
+      monthly_recesses = result.map {|row| row["monthly_recesses"].to_i }
+      # 累積数
+      total_increase = result.map {|row| row["total_increase"].to_i }
+      total_decrease = result.map {|row| row["total_decrease"].to_i }
+      total_changes = [total_increase, total_decrease].transpose.map {|num| num.inject(&:-) }
+      if monthly_recesses.present?
+        total_changes = [total_changes, monthly_recesses].transpose.map {|num| num.inject(&:-) }
+      end
+      {
+        months: months,
+        monthly_increase: monthly_increase,
+        monthly_decrease: monthly_decrease,
+        monthly_recesses: monthly_recesses,
+        total_changes: total_changes
+      }
+    end
+  
 end
--- app/queries/statistics_query.rb
+++ app/queries/statistics_query.rb
@@ -1,12 +1,10 @@
 class StatisticsQuery
 
-  @@begin_month = '201306'
-
   def self.monthly_active_members
     end_month = Date.today.strftime('%Y%m')
     query = <<-EOS.strip_heredoc
       WITH
-        #{month_period_query(month_from: @@begin_month, month_to: end_month)},
+        #{worked_months_query()},
         members_abbr AS (
           SELECT
             id,
@@ -17,12 +15,12 @@ class StatisticsQuery
         )
       SELECT
         month,
-        (SELECT COUNT(*) FROM members_abbr WHERE enter_month = month_period.month) AS monthly_increase,
-        (SELECT COUNT(*) FROM members_abbr WHERE enter_month <= month_period.month) AS total_increase,
-        (SELECT COUNT(*) FROM members_abbr WHERE leave_month = month_period.month) AS monthly_decrease,
-        (SELECT COUNT(*) FROM members_abbr WHERE leave_month < month_period.month) AS total_decrease
+        (SELECT COUNT(*) FROM members_abbr WHERE enter_month = worked_months.month) AS monthly_increase,
+        (SELECT COUNT(*) FROM members_abbr WHERE enter_month <= worked_months.month) AS total_increase,
+        (SELECT COUNT(*) FROM members_abbr WHERE leave_month = worked_months.month) AS monthly_decrease,
+        (SELECT COUNT(*) FROM members_abbr WHERE leave_month < worked_months.month) AS total_decrease
       FROM
-        month_period
+        worked_months
       ORDER BY
         month
     EOS
@@ -33,7 +31,7 @@ class StatisticsQuery
     end_month = Date.today.strftime('%Y%m')
     query = <<-EOS.strip_heredoc
       WITH
-        #{month_period_query(month_from: @@begin_month, month_to: end_month)},
+        #{worked_months_query()},
         members_courses_abbr AS (
           SELECT
              members_courses.id,
@@ -62,35 +60,78 @@ class StatisticsQuery
         )
       SELECT
         month,
-        (SELECT COUNT(*) FROM members_courses_abbr WHERE begin_month = month_period.month) AS monthly_increase,
-        (SELECT COUNT(*) FROM members_courses_abbr WHERE begin_month <= month_period.month) AS total_increase,
-        (SELECT COUNT(*) FROM studios_recesses WHERE month = month_period.month) AS monthly_recesses,
-        (SELECT COUNT(*) FROM members_courses_abbr WHERE end_month = month_period.month) AS monthly_decrease,
-        (SELECT COUNT(*) FROM members_courses_abbr WHERE end_month < month_period.month) AS total_decrease
+        (SELECT COUNT(*) FROM members_courses_abbr WHERE begin_month = worked_months.month) AS monthly_increase,
+        (SELECT COUNT(*) FROM members_courses_abbr WHERE begin_month <= worked_months.month) AS total_increase,
+        (SELECT COUNT(*) FROM studios_recesses WHERE month = worked_months.month) AS monthly_recesses,
+        (SELECT COUNT(*) FROM members_courses_abbr WHERE end_month = worked_months.month) AS monthly_decrease,
+        (SELECT COUNT(*) FROM members_courses_abbr WHERE end_month < worked_months.month) AS total_decrease
       FROM
-        month_period
+        worked_months
       ORDER BY
         month
     EOS
     ActiveRecord::Base.connection.execute(query)
   end
+  
+  def self.sales_report(school_id:)
+    end_month = Date.today.strftime('%Y%m')
+    query = <<-EOS.strip_heredoc
+      WITH
+        #{worked_months_query()},
+        members_courses_abbr AS (
+          SELECT
+             members_courses.id,
+             TO_CHAR(members_courses.begin_date, 'YYYYmm') AS begin_month,
+             TO_CHAR(members_courses.end_date, 'YYYYmm') AS end_month,
+             courses.monthly_fee
+          FROM
+            members_courses
+          INNER JOIN courses
+            ON members_courses.course_id = courses.id
+          INNER JOIN timetables
+            ON courses.timetable_id = timetables.id
+          INNER JOIN studios
+            ON timetables.studio_id = studios.id
+          INNER JOIN schools
+            ON studios.school_id = schools.id
+          WHERE
+            schools.id = #{school_id}
+        )
+      SELECT
+        month,
+        (SELECT
+           SUM(monthly_fee)
+         FROM
+           members_courses_abbr
+         WHERE
+           begin_month <= worked_months.month
+           AND (end_month IS NULL OR end_month > worked_months.month)
+        ) AS sales
+      FROM
+        worked_months
+      ORDER BY
+        month
+    EOS
+    ActiveRecord::Base.connection.execute(query)
+
+  end
 
   private
   
-  def self.month_period_query(month_from: '201306', month_to: Date.today.strftime('%Y%m'))
-    query = <<-EOS.strip_heredoc
-      RECURSIVE tmp(timestamp) AS (
-        SELECT TO_TIMESTAMP(\'#{month_from}01 000000\', 'YYYYMMDD HH24MISS') AS timestamp
-        UNION ALL
-        SELECT tmp.timestamp + '1 months' FROM tmp WHERE TO_CHAR(tmp.timestamp, 'YYYYMM') < \'#{month_to}\'
-      ),
-      month_period AS (
-        SELECT
-          TO_CHAR(timestamp, 'YYYYMM') AS month
-        FROM
-          tmp
-      )
-    EOS
-  end
+    def self.worked_months_query(month_from: Rollbook::Util::Month.total_worked_months.first, month_to: Rollbook::Util::Month.total_worked_months.last)
+      query = <<-EOS.strip_heredoc
+        RECURSIVE tmp(timestamp) AS (
+          SELECT TO_TIMESTAMP(\'#{month_from}01 000000\', 'YYYYMMDD HH24MISS') AS timestamp
+          UNION ALL
+          SELECT tmp.timestamp + '1 months' FROM tmp WHERE TO_CHAR(tmp.timestamp, 'YYYYMM') < \'#{month_to}\'
+        ),
+        worked_months AS (
+          SELECT
+            TO_CHAR(timestamp, 'YYYYMM') AS month
+          FROM
+            tmp
+        )
+      EOS
+    end
 
 end
