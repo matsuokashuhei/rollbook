33d3bd35fc0899500622733f5572431769955b1b
matsuosh <shooohei.m@gmail.com>
2014-12-02 23:28:46 +0900

    [modify] 消費税を一律で8%にしていたが5%または8%にするように変えた。

--- lib/rollbook/money.rb
+++ lib/rollbook/money.rb
@@ -2,17 +2,15 @@ module Rollbook
 
   module Money
   
-    # 消費税の税率  
-    @@consumption_tax_rate = 0.08
-
     # 消費税を計算する。
-    def self.calculate_consumption_tax(amount)
-      calculate_tax(amount, @@consumption_tax_rate)
+    def self.calculate_consumption_tax(amount, date = Date.today)
+      tax_rate = consumption_tax_rate(date: date)
+      calculate_tax(amount, tax_rate)
     end
 
     # 消費税込の金額にする。
-    def self.include_consumption_tax(amount)
-      amount + calculate_consumption_tax(amount)
+    def self.include_consumption_tax(amount, date = Date.today)
+      amount + calculate_consumption_tax(amount, date)
     end
 
     # 源泉徴収税の税率
@@ -30,13 +28,18 @@ module Rollbook
 
     private
     
-    def self.calculate_tax(amount, tax_rate)
-      if amount > 0
-        (amount * tax_rate).truncate
-      else
-        0
+      def self.calculate_tax(amount, tax_rate)
+        if amount > 0
+          (amount * tax_rate).truncate
+        else
+          0
+        end
       end
-    end
+
+      def self.consumption_tax_rate(date: Date.today)
+        return 0.05 if date < Date.new(2014, 4, 1)
+        return 0.08
+      end
 
   end
 
