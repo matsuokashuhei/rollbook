3ca2924ae0c89d6246dd5eb07ee46072317c9b6d
Shuhei Matsuoka <matsuoka.syuhei@hamee.co.jp>
2018-09-25 00:27:17 +0900

    Updated some files by calling rails app:update

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,5 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require_relative 'application'
 
 # Initialize the Rails application.
-Rollbook::Application.initialize!
-
-=begin
-ActionMailer::Base.stmp_settings = {
-  address: "smtp.sendgred.net",
-  port: "587",
-  authentication: :plain,
-  user_name: ENV["SENDGRID_USERNAME"],
-  password: ENV["SENDGRID_PASSWORD"],
-  domain: "heroku.com",
-  enable_starttls_auto: true
-}
-=end
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rollbook::Application.config.session_store :cookie_store, key: '_rollbook_session'
+Rails.application.config.session_store :cookie_store, key: '_rollbook_session'
