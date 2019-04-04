cb62fdd253525a77481f33ec8fde45b98913b079
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2019-04-04 17:16:45 +0900

    Added aliases for development

---
+++ alias.sh
@@ -0,0 +1,4 @@
+alias rails='docker-compose run --rm app rails'
+alias bundle='docker-compose run --rm app bundle'
+alias app='docker-compose run -- rm app'
+alias webpacker='docker-compose run --rm app rails webpacker'
