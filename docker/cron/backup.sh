9a107cd62abae1b825a7094bf8f831197217f096
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2019-04-06 03:26:08 +0900

    Addes a https-portal container

--- docker/cron/backup.sh
+++ docker/cron/backup.sh
@@ -1,36 +1,41 @@
 #!/bin/bash -eu
 
+function log() {
+  echo "$(date '+%Y-%m-%dT%H:%M:%S') - $@"
+}
+
 source /root/env.sh
 
 NEW_DUMP=/backup/$(date '+%Y%m%d')-${DB_NAME}.dump
 OLD_DUMP=/backup/$(date --date '7 day ago' '+%Y%m%d')-${DB_NAME}.dump
 
 # backup DB using pg_dump
-echo "$(date '+%Y-%m-%d %H:%M:%S') - pg_dump --format=custom --host=$DB_HOST --dbname=$DB_NAME --no-owner --no-acl --username=$DB_USER > $NEW_DUMP" >> /var/log/cron.log 2>&1
+log "pg_dump --format=custom --host=$DB_HOST --dbname=$DB_NAME --no-owner --no-acl --username=$DB_USER > $NEW_DUMP"
 pg_dump --format=custom --host=$DB_HOST --dbname=$DB_NAME --no-owner --no-acl --username=$DB_USER > $NEW_DUMP
 
 if [ $RAILS_ENV = 'production' ]
 then
   # store dump into S3
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - aws s3 cp $NEW_DUMP s3://${S3_BUCKET}/backup/" >> /var/log/cron.log 2>&1
+  log "aws s3 cp $NEW_DUMP s3://${S3_BUCKET}/backup/"
   aws s3 cp $NEW_DUMP s3://${S3_BUCKET}/backup/
   if [ $? -eq 0 ]
   then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - aws s3 cp is succeeded" >> /var/log/cron.log 2>&1
+    log "aws s3 cp is succeeded"
 
     # remove dump from container
-    #echo "$(date '+%Y-%m-%d %H:%M:%S') - rm -fr $NEW_DUMP" >> /var/log/cron.log 2>&1
+    #log "rm -fr $NEW_DUMP" >> /var/log/cron.log 2>&1
     #rm -fr $NEW_DUMP
 
     # remove old dump from S3
     rm -f $OLD_DUMP
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - aws s3 rm s3://${S3_BUCKET}/$OLD_DUMP" >> /var/log/cron.log 2>&1
+    log "aws s3 rm s3://${S3_BUCKET}/$OLD_DUMP"
     aws s3 rm s3://${S3_BUCKET}/backup/$OLD_DUMP
     if [ $? -eq 0 ]
     then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - aws s3 rm is succeeded" >> /var/log/cron.log 2>&1
+      log "aws s3 rm is succeeded"
     fi
   fi
 fi
+
 
 
