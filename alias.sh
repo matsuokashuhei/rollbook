dd4cceca6125a63e6a6f62c9edf4f110ead44804
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2019-04-06 01:30:13 +0900

    Upated alias.sh

--- alias.sh
+++ alias.sh
@@ -1,5 +1,11 @@
-alias docker-compose='docker-compose -f docker-compose.yml -f docker-compose-development.yml'
-alias docker-compose-production='docker-compose -f docker-compose.yml -f docker-compose-production.yml'
+#!/bin/sh -eu
+
+if [ $(uname -s) = 'Darwin' ]
+then
+  alias docker-compose='docker-compose -f docker-compose.yml -f docker-compose-development.yml'
+else
+  alias docker-compose='docker-compose -f docker-compose.yml -f docker-compose-production.yml'
+fi
 alias rails='docker-compose run --rm app rails'
 alias bundle='docker-compose run --rm app bundle'
 alias app='docker-compose run -- rm app'
